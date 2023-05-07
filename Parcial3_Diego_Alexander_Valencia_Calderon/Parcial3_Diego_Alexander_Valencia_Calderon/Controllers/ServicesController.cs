2480eeb26474ba404b561ceab183a75dee8cea68
DIEGO ALEXANDER VALENCIA CALDER�N <diegovalencia313667@correo.itm.edu.co>
2023-05-07 01:51:37 -0500

    I created a ServicesController.cs and a SeederDb.cs

---
+++ Parcial3_Diego_Alexander_Valencia_Calderon/Parcial3_Diego_Alexander_Valencia_Calderon/Controllers/ServicesController.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Parcial3_Diego_Alexander_Valencia_Calderon.DAL;
+using Parcial3_Diego_Alexander_Valencia_Calderon.DAL.Entities;
+
+namespace Parcial3_Diego_Alexander_Valencia_Calderon.Controllers
+{
+     public class ServicesController : Controller
+     {
+        #region Constructor
+
+        private readonly DatabaseContext _context;
+
+          public ServicesController(DatabaseContext context)
+          {
+              _context = context;
+          }
+
+        #endregion
+
+        #region Methods
+
+        // GET: Services
+        public async Task<IActionResult> Index()
+        {
+            return _context.Services != null ?
+                        View(await _context.Services.ToListAsync()) :
+                        Problem("Entity set 'DatabaseContext.Services'  is null.");
+        }
+
+        // GET: Services/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null || _context.Services == null)
+            {
+                return NotFound();
+            }
+
+            var service = await _context.Services
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (service == null)
+            {
+                return NotFound();
+            }
+
+            return View(service);
+        }
+
+        #endregion region
+
+        #region Actions
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Services/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Price")] Service service)
+        {
+            if (ModelState.IsValid)
+            {
+                service.Id = Guid.NewGuid();
+                _context.Add(service);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(service);
+        }
+
+        // GET: Services/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null || _context.Services == null)
+            {
+                return NotFound();
+            }
+
+            var service = await _context.Services.FindAsync(id);
+            if (service == null)
+            {
+                return NotFound();
+            }
+            return View(service);
+        }
+
+        // POST: Services/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("Name,Price,Id")] Service service)
+        {
+            if (id != service.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(service);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ServiceExists(service.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(service);
+        }
+
+        // GET: Services/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null || _context.Services == null)
+            {
+                return NotFound();
+            }
+
+            var service = await _context.Services
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (service == null)
+            {
+                return NotFound();
+            }
+
+            return View(service);
+        }
+
+        // POST: Services/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            if (_context.Services == null)
+            {
+                return Problem("Entity set 'DatabaseContext.Services'  is null.");
+            }
+            var service = await _context.Services.FindAsync(id);
+            if (service != null)
+            {
+                _context.Services.Remove(service);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ServiceExists(Guid id)
+        {
+            return (_context.Services?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+        #endregion
+    }
+}
---
+++ Parcial3_Diego_Alexander_Valencia_Calderon/Parcial3_Diego_Alexander_Valencia_Calderon/Controllers/WashingCarDBContext.cs
@@ -0,0 +1,10 @@
+﻿namespace Parcial3_Diego_Alexander_Valencia_Calderon.Controllers
+{
+    public class WashingCarDBContext
+    {
+        internal Task SaveChangesAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
